c6b9d70b4de9fa3c19fd2451b2e722ba45ec0ee5
Hlwam Moe Naing <hlwammoe.ak@gmail.com>
2023-06-10 20:48:27 +0630

    Add: IntroScreen Finish

--- app/src/main/java/com/homeland/pay/ui/activities/MainActivity.kt
+++ app/src/main/java/com/homeland/pay/ui/activities/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.homeland.pay.ui.activities
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.navigation.fragment.NavHostFragment
 import com.homeland.pay.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+
+        val navController = navHostFragment.navController
+        val navGraph = navController.graph
+
+        Log.d("@onBoTst",onBoardingFinished().toString())
+        if (onBoardingFinished()){
+            navGraph.startDestination = R.id.homeFragment
+            navController.graph = navGraph
+
+        }else{
+            navGraph.startDestination = R.id.splashFragment
+            navController.graph = navGraph
+
+        }
+
+    }
+
+    private fun onBoardingFinished(): Boolean{
+        val sharedPref = this.getSharedPreferences("onBoarding", Context.MODE_PRIVATE)
+        return sharedPref.getBoolean("Finished", false)
     }
 }
