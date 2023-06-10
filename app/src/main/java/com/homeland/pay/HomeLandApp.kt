b4947198f44299dd7e79bab4f7a5366d2cdb87e3
Hlwam_Moe_Naing <hlwammoe.ak@gmail.com>
2023-06-11 00:51:32 +0630

    Initial commit

---
+++ app/src/main/java/com/homeland/pay/HomeLandApp.kt
@@ -0,0 +1,9 @@
+package com.homeland.pay
+
+import android.app.Application
+
+class HomeLandApp:Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/homeland/pay/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.homeland.pay.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.homeland.pay.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
